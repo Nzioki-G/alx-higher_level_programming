2a3a690ebfcd16966371121dbd1609707ae13491
overwatch <nzioki.grace@gmail.com>
2022-06-28 22:24:34 +0300

    lower to uppercase

---
+++ 0x01-python-if_else_loops_functions/8-uppercase.py
@@ -0,0 +1,8 @@
+def uppercase(str):
+    new_str = ""
+
+    for ch in str:
+        if ord(ch) >= 97 and ord(ch) <= 123:
+            ch = chr(ord(ch) - 32)
+        new_str = new_str + ch
+    print("{}".format(new_str))
