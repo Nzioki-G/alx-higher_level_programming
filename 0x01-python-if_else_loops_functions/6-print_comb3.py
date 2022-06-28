77cb5b44963d5d0b82e52312356a64de23583765
overwatch <nzioki.grace@gmail.com>
2022-06-28 21:45:41 +0300

    2-digit different combinations

--- 0x01-python-if_else_loops_functions/6-print_comb3.py
+++ 0x01-python-if_else_loops_functions/6-print_comb3.py
@@ -1,12 +1,12 @@
 #!/usr/bin/python3
 
-#89 is the largest of these numbers
+# 89 is the largest of these numbers
 for i in range(1, 89):
 
-  # switch places ex. 17 -> 71 then take the smaller
-  if ((i/10 + (i % 10) * 10) < i) or i % 11 == 0:
-    continue
+    # switch places ex. 17 -> 71 then take the smaller
+    if ((i/10 + (i % 10) * 10) < i) or i % 11 == 0:
+        continue
 
-  print("{:>02}, ".format(i), end="")
+    print("{:>02}, ".format(i), end="")
 
 print("{}".format(89))
