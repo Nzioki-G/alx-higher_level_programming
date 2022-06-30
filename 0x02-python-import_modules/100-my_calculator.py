07db4a0522cc174603e3d4ec73058f4d7b103479
overwatch <nzioki.grace@gmail.com>
2022-07-01 01:36:04 +0300

    simple calc

---
+++ 0x02-python-import_modules/100-my_calculator.py
@@ -0,0 +1,26 @@
+#!/usr/bin/python3
+if __name__ == "__main__":
+    from calculator_1 import add, sub, mul, div
+    from sys import argv
+
+    operations = ['+', '-', '*', '/']
+
+    if not(len(argv) == 4):
+        print("Usage: ./100-my_calculator.py <a> <operator> <b> ")
+        exit(1)
+    else:
+        a = int(argv[1])
+        operator = argv[2]
+        b = int(argv[3])
+        if operator not in operations:
+            print("Unknown operator. Available operators: +, -, * and /")
+            exit(1)
+        else:
+            if operator == '+':
+                print("{} + {} = {}".format(a, b, add(a, b)))
+            elif operator == '-':
+                print("{} - {} = {}".format(a, b, sub(a, b)))
+            elif operator == '*':
+                print("{} * {} = {}".format(a, b, mul(a, b)))
+            else:
+                print("{} / {} = {}".format(a, b, div(a, b)))
