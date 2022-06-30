e67d67eabde257a84d0fa8498f937d7f4f03b3ed
overwatch <nzioki.grace@gmail.com>
2022-06-30 20:22:52 +0300

    argv

---
+++ 0x02-python-import_modules/2-args.py
@@ -0,0 +1,16 @@
+#!/usr/bin/python3
+if __name__ == "__main__":
+    from sys import argv
+    print("{} arguement".format(len(argv) - 1), end="")
+
+    # no args
+    if len(argv) == 1:
+        print("s.")
+    # we have more than 1 args
+    elif len(argv) > 2:
+        print("s:")
+    # only one arg
+    else:
+        print(":")
+    for i in range(1, len(argv)):
+        print("{}: {}".format(i, argv[i]))
