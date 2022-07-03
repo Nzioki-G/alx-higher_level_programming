3940561dafdfd1483a8ae88f49d2f4096bf83c86
overwatch <nzioki.grace@gmail.com>
2022-07-04 02:32:18 +0300

    print matrix

---
+++ 0x03-python-data_structures/6-print_matrix_integer.py
@@ -0,0 +1,9 @@
+#!/usr/bin/python3
+def print_matrix_integer(matrix=[[]]):
+    x = 1
+    for sub_array in matrix:
+        for i in sub_array:
+            str = " " if (i != len(sub_array) * x) else ""
+            print(i, end=str)
+        x += 1
+        print()
