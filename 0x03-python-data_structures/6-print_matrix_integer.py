1b8484a32ec1e4248fa6ef5f013d2966615e36be
overwatch <nzioki.grace@gmail.com>
2022-07-04 02:55:31 +0300

    print matrix

--- 0x03-python-data_structures/6-print_matrix_integer.py
+++ 0x03-python-data_structures/6-print_matrix_integer.py
@@ -1,9 +1,11 @@
 #!/usr/bin/python3
 def print_matrix_integer(matrix=[[]]):
-    x = 1
     for sub_array in matrix:
+        x = 0
         for i in sub_array:
-            str = " " if (i != len(sub_array) * x) else ""
-            print("{:d}".format(i), end=str)
-        x += 1
+            if x == 0:
+                print("{:d}".format(i), end="")
+            else:
+                print("{:< 1d}".format(i), end="")
+            x += 1
         print()
