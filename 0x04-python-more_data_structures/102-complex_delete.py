ce06d1735fdc863654d547e4fce0d72442394fa0
overwatch <nzioki.grace@gmail.com>
2022-07-07 04:22:43 +0300

    delete from dict by values

---
+++ 0x04-python-more_data_structures/102-complex_delete.py
@@ -0,0 +1,9 @@
+#!/usr/bin/python3
+def complex_delete(a_dictionary, value):
+
+    for k in a_dictionary.copy():
+        if a_dictionary[k] == value:
+            del a_dictionary[k]
+        else:
+            continue
+    return a_dictionary
