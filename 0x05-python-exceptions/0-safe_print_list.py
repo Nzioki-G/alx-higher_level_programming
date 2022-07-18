b08e8b8fcacaf0612bf638979c749c999a0ce850
overwatch <nzioki.grace@gmail.com>
2022-07-18 20:09:15 +0300

    handle IndexError Exception

--- 0x05-python-exceptions/0-safe_print_list.py
+++ 0x05-python-exceptions/0-safe_print_list.py
@@ -3,13 +3,12 @@ def safe_print_list(my_list=[], x=0):
     num = 0
     try:
         if x > 0:
-            for i in my_list:
-                print(i, end="")
+            for i in range(x):
+                print(my_list[i], end="")
                 num += 1
-                if num == x:
-                    break
             print()
         return num
 
     except IndexError:
+        print()
         return num
