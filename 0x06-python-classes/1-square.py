bb17d16e01545d331e9cf53945fe91596955e989
overwatch <nzioki.grace@gmail.com>
2022-07-19 23:19:06 +0300

    initaialize square

---
+++ 0x06-python-classes/1-square.py
@@ -0,0 +1,12 @@
+#!/usr/bin/python3
+"""A blueprint of a square"""
+
+
+class Square:
+    """
+    class that defines and initializes a square object
+    """
+    __size = 0
+
+    def __init__(self, size):
+        self.__size = size
