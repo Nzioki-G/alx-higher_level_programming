03c6bc83c22015c3e406ab7beefdc04e9e25629c
overwatch <nzioki.grace@gmail.com>
2022-07-19 23:51:43 +0300

    position square in 2D?

--- 0x06-python-classes/6-square.py
+++ 0x06-python-classes/6-square.py
@@ -93,7 +93,7 @@ class Square:
 
         if self.__position[1] > 0:
             print()
-        #print(" " * self.__position[1])
+
         for i in range(self.__size):
             print(" " * self.__position[0] + "#" * self.__size)
 
