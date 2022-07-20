a9fa85c50e43e057e4b19de7b8716c2ec04547c9
overwatch <nzioki.grace@gmail.com>
2022-07-20 18:45:01 +0300

    magic calculation withh classes

---
+++ 0x06-python-classes/103-magic_class.py
@@ -0,0 +1,20 @@
+#!/usr/bin/python3
+class MagicClass:
+    import math
+
+    def __init__(self, radius=0):
+        self.__radius = 0
+
+        if type(radius) != int and type(radius) != float:
+            raise TypeError("radius must be a number")
+
+        else:
+            self.__radius = radius
+
+    def area(self):
+        """calcs the area of a circle"""
+        return self.__radius ** 2 * math.pi
+
+    def circumference(self):
+        """calcs the perimeter of a circle"""
+        return 2 * math.pi * self.__radius
