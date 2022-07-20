9167a8492c5328d5cd2b22f8fd3b6ac47dc76c68
overwatch <nzioki.grace@gmail.com>
2022-07-20 17:18:13 +0300

    print a square with # and its position with spaces

--- 0x06-python-classes/6-square.py
+++ 0x06-python-classes/6-square.py
@@ -93,11 +93,8 @@ class Square:
 
         if self.__size == 0:
             print("")
-        if self.__position[1] > 0:
-            print()
-
-        for i in range(self.__size):
-            print(" " * self.__position[0] + "#" * self.__size)
-
-        if self.__size == 0:
-            print()
+        else:
+            for i in range(self.__position[1]):
+                print()
+            for i in range(self.__size):
+                print((" " * self.__position[0]) + ("#" * self.__size))
