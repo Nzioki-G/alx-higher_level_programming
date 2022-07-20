7f7d1f2aab6bd52e6cd7bfeb9a8ae7f7173f7188
overwatch <nzioki.grace@gmail.com>
2022-07-20 19:07:23 +0300

    magic calculation withh classes

--- 0x06-python-classes/103-magic_class.py
+++ 0x06-python-classes/103-magic_class.py
@@ -2,17 +2,24 @@
 """A circle in code"""
 
 
+import math
+import dis
+
+
 class MagicClass:
-    """implements a circle, gets area and perimeter"""
-    import math
+    """implements a circle with area & perimeter"""
 
     def __init__(self, radius=0):
         self.__radius = 0
 
-        if type(radius) != int and type(radius) != float:
+        if (type(radius) != int
+                and type(radius) != float):
+
             raise TypeError("radius must be a number")
 
         self.__radius = radius
+
+        return
 
     def area(self):
         """calcs the area of a circle"""
@@ -23,7 +30,5 @@ class MagicClass:
         return 2 * math.pi * self.__radius
 
 
-'''
-import dis
-c = MagicClass(4)
+'''c = MagicClass(9)
 dis.dis(c.__init__)'''
