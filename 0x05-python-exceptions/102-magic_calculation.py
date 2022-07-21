2eeb7679e91d1f9f08306672904c178d22a920fc
overwatch <nzioki.grace@gmail.com>
2022-07-21 20:44:05 +0300

    disassembling with exceptions

--- 0x05-python-exceptions/102-magic_calculation.py
+++ 0x05-python-exceptions/102-magic_calculation.py
@@ -1,11 +1,13 @@
 #!/usr/bin/python3
 def magic_calculation(a, b):
     result = 0
-
     for i in range(1, 3):
         try:
             if i > a:
-                b = i ** a
-                result += (a ** b) / i
-        except Exception:
-            raise
+                raise Exception("Too far")
+
+            result += (a ** b) / i
+        except:
+            result = a + b
+
+    return result
