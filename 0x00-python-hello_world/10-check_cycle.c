52191c83a169a184e6c5310b2c0edc20e4e01605
Grace <nzioki.grace@gmail.com>
2023-08-07 22:07:18 +0300

    check linked list cycle

---
+++ 0x00-python-hello_world/10-check_cycle.c
@@ -0,0 +1,37 @@
+#include "lists.h"
+
+/**
+ * check_cycle - checks if a singly linked list has a cycle in it
+ * @list: the linked list to ckeck for cycle
+ *
+ * Return: 0 if there is no cycle, 1 if there is a cycle
+ */
+int check_cycle(listint_t *list)
+{
+	listint_t *one, *two;
+
+	if (list == NULL)
+		return (0);
+
+	one = list->next;
+	if (one == NULL)
+		return (0);
+	two = one->next;
+
+	while (one != two && two != NULL)
+	{
+		/* take 1 step */
+		one = one->next;
+
+		/* take 2 steps */
+		two = two->next;
+		if (two)
+			two = two->next;
+		
+		/* if our nodes collide, we have a cycle */
+		if (one == two)
+			return (1);
+	}
+
+	return (0);
+}
