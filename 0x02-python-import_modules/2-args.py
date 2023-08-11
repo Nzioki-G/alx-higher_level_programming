ad4c4b5e4e19d17e8415ac7d66629316ef91f7a7
Grace <nzioki.grace@gmail.com>
2023-08-11 23:43:35 +0300

    do not execute on import: __name__ == "__main__"

--- 0x02-python-import_modules/2-args.py
+++ 0x02-python-import_modules/2-args.py
@@ -1,13 +1,14 @@
 #!/usr/bin/python3
-from sys import argv
-argc = len(argv)
+if __name__ == "__main__":
+    from sys import argv
+    argc = len(argv)
 
-if argc == 1:
-    print("0 arguments.")
+    if argc == 1:
+        print("0 arguments.")
 
-else:
-    str = "argument"
-    str += "s:" if argc > 2 else ":"
-    print("{} {}".format(argc - 1, str))
-    for ag in range(1, argc):
-        print("{}: {}".format(ag, argv[ag]))
+    else:
+        str = "argument"
+        str += "s:" if argc > 2 else ":"
+        print("{} {}".format(argc - 1, str))
+        for ag in range(1, argc):
+            print("{}: {}".format(ag, argv[ag]))
