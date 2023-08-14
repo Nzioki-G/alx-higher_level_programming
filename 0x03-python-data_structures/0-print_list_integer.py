fc4f6a868a1a28716475281b1eee57d3ae855e58
Grace <nzioki.grace@gmail.com>
2023-08-15 01:01:43 +0300

    print list forward, in reverse

--- 0x03-python-data_structures/0-print_list_integer.py
+++ 0x03-python-data_structures/0-print_list_integer.py
@@ -1,4 +1,10 @@
-#!/usr/bin/python
+#!/usr/bin/python3
 def print_list_integer(my_list=[]):
     for integer in my_list:
-        print("{}".format(integer))
+        print("{:d}".format(integer))
+
+
+"""
+my_list = [1, 2, 3, 4, 5]
+print_list_integer(my_list)
+"""
--- 0x03-python-data_structures/3-print_reversed_list_integer.py
+++ 0x03-python-data_structures/3-print_reversed_list_integer.py
@@ -2,4 +2,10 @@
 def print_reversed_list_integer(my_list=[]):
     last = len(my_list)
     for idx in range(last - 1, -1, -1):
-        print("{}".format(my_list[idx]))
+        print("{:d}".format(my_list[idx]))
+
+
+"""
+my_list = [1, 2, 3, 4, 5]
+print_reversed_list_integer(my_list)
+"""
