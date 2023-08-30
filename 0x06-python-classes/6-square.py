c396ea0952563f397cc9085777fa6be8758112da
Grace <nzioki.grace@gmail.com>
2023-08-30 18:11:20 +0300

    print square on axis

--- 0x06-python-classes/6-square.py
+++ 0x06-python-classes/6-square.py
@@ -39,6 +39,8 @@ class Square:
         """sets/updates the position of a square"""
         if not isinstance(value, tuple) or len(value) != 2:
             raise TypeError("position must be a tuple of 2 positive integers")
+        if value[0] < 0 or value[1] < 0 or value[0] != int or value[1] != int:
+            raise TypeError("position must be a tuple of 2 positive integers")
         self.__position = value
 
     def my_print(self):
@@ -69,7 +71,7 @@ my_square_2.my_print()
 
 print("--")
 
-my_square_3 = Square(3, (3, 0))
+my_square_3 = Square(3, (1, "3"))
 my_square_3.my_print()
 
 print("--")
