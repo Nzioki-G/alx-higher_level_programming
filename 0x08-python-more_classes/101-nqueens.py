b73fd9a240b96873ce61efb6af7c4ca64a125bba
Grace <nzioki.grace@gmail.com>
2023-09-04 20:57:44 +0300

    place N queens on NxN chessboard

--- 0x08-python-more_classes/101-nqueens.py
+++ 0x08-python-more_classes/101-nqueens.py
@@ -5,21 +5,22 @@
 import sys
 
 
-def non_attacking_queens():
-    '''place N none-attacking queens on an N x N chessboard'''
-    if len(sys.argv) != 2:
-        print("Usage: nqueens N")
-        exit(1)
-    n = sys.argv[1]
-    if not isinstance(n, int):
-        print("N must be a number")
-        exit(1)
-    if n < 4:
-        print("N must be at least 4")
-        exit(1)
-    recursive_helper(n)
-
 def recursive_helper(n):
     '''place queen, check for collisions, backtrack repeat'''
     pass
 
+
+'''place N none-attacking queens on an N x N chessboard'''
+if len(sys.argv) != 2:
+    print("Usage: nqueens N")
+    exit(1)
+n = sys.argv[1]
+try:
+    n = int(n)
+except ValueError:
+    print("N must be a number")
+    exit(1)
+if n < 4:
+    print("N must be at least 4")
+    exit(1)
+recursive_helper(n)
