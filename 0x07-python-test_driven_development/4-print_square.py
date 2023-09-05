3450ea85de8ca9a005c6ea5c7f5e44bea88aff6f
Grace <nzioki.grace@gmail.com>
2023-09-05 21:23:20 +0300

    print square

---
+++ 0x07-python-test_driven_development/4-print_square.py
@@ -0,0 +1,29 @@
+#!/usr/bin/python3
+"""Print square"""
+
+
+def print_square(size):
+    """prints a square with the character #"""
+    if not isinstance(size, int):
+        raise TypeError("size must be an integer")
+    if size < 0:
+        raise ValueError("size must be >= 0")
+    for _ in range(size):
+        print('#' * size)
+
+
+"""
+print_square(4)
+print("")
+print_square(10)
+print("")
+print_square(0)
+print("")
+print_square(1)
+print("")
+try:
+    print_square(-1)
+except Exception as e:
+    print(e)
+print("")
+"""
