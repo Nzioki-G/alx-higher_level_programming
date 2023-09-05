5f062e0275d07c826aa32be74053c040cb0f39ba
Grace <nzioki.grace@gmail.com>
2023-09-05 21:21:47 +0300

    int addition

---
+++ 0x07-python-test_driven_development/0-add_integer.py
@@ -0,0 +1,27 @@
+#!/usr/bin/python3
+"""integer addition"""
+
+
+def add_integer(a, b=98):
+    '''a function that adds 2 integers'''
+    if not isinstance(a, int) and not isinstance(a, float):
+        raise TypeError("a must be an integer")
+    if not isinstance(b, int) and not isinstance(b, float):
+        raise TypeError("b must be an integer")
+    return int(a) + int(b)
+
+
+"""
+print(add_integer(pi))
+print(add_integer(100, -2))
+print(add_integer(2))
+print(add_integer(100.3, -2))
+try:
+    print(add_integer(4, "School"))
+except Exception as e:
+    print(e)
+try:
+    print(add_integer(None))
+except Exception as e:
+    print(e)
+"""
