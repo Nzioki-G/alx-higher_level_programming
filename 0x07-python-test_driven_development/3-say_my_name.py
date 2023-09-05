af92ee0a2f6f7f0dda4230846062f6373427d440
Grace <nzioki.grace@gmail.com>
2023-09-05 21:22:56 +0300

    say my name

---
+++ 0x07-python-test_driven_development/3-say_my_name.py
@@ -0,0 +1,22 @@
+#!/usr/bin/python3
+"""Say my name"""
+
+
+def say_my_name(first_name, last_name=""):
+    """prints name in full given first and optionally last name"""
+    if not isinstance(first_name, str):
+        raise TypeError("first_name must be a string")
+    if not isinstance(last_name, str):
+        raise TypeError("last_name must be a string")
+    print("My name is {} {}".format(first_name, last_name))
+
+
+"""
+say_my_name("John", "Smith")
+say_my_name("Walter", "White")
+say_my_name("Bob")
+try:
+    say_my_name(12, "White")
+except Exception as e:
+    print(e)
+"""
