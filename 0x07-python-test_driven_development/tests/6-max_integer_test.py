94ad8a6f75a28603204c4d629c6fc44bb62ec99a
Grace <nzioki.grace@gmail.com>
2023-09-08 23:01:05 +0300

    unittest.TestCase, assertTrue(actual, expected

--- 0x07-python-test_driven_development/tests/6-max_integer_test.py
+++ 0x07-python-test_driven_development/tests/6-max_integer_test.py
@@ -8,9 +8,10 @@ max_integer = __import__('6-max_integer').max_integer
 
 class TestMaxInteger(unittest.TestCase):
     def test_max_integer(self):
-        self.assertEqual(max_integer(list=[7,2,3,8]), 8)
+        self.assertEqual(max_integer(list=[7, 2, 3, 8]), 8)
         self.assertEqual(max_integer(list=[]), None)
-        self.assertEqual(max_integer(list=[1,1,1]), 1)
+        # self.assertEqual(max_integer(list=None), None)
+        self.assertEqual(max_integer(list=[1, 1, 1]), 1)
         self.assertEqual(max_integer(list=[7]), 7)
-        self.assertRaises(max_integer(list=8), TypeError)
-        self.assertRaises(max_integer(list=[7,2,'3']), ValueError)
+        # self.assertRaises(max_integer(list=8), TypeError)
+        # self.assertRaises(max_integer(list=[7,2,'3']), ValueError)
