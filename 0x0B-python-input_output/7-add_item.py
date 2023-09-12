c4da140c857bd57cf9fd47c8ef4f103aadfce3df
Grace <nzioki.grace@gmail.com>
2023-09-12 18:14:59 +0300

    load old, append new, save to file

--- 0x0B-python-input_output/7-add_item.py
+++ 0x0B-python-input_output/7-add_item.py
@@ -5,14 +5,14 @@
 import sys
 
 
-save = __import__('5-save_to_json_file').save_to_json_file
-load = __import__('6-load_from_json_file').load_from_json_file
+save_to_json_file = __import__('5-save_to_json_file').save_to_json_file
+load_from_json_file = __import__('6-load_from_json_file').load_from_json_file
 
 # fetch any data in file
-my_list = load("add_item.json")
+my_list = load_from_json_file("add_item.json")
 
 # append the args to fetched data
 my_list += sys.argv[1:]
 
 # save new list to file
-save(my_list, "add_item.json")
+save_to_json_file(my_list, "add_item.json")
