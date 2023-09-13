983956642a19ef168c210bd5c7307e38c0c2eeaf
Grace <nzioki.grace@gmail.com>
2023-09-13 19:57:28 +0300

    object to dictionary

---
+++ 0x0B-python-input_output/8-class_to_json.py
@@ -0,0 +1,72 @@
+#!/usr/bin/python3
+'''Class to simple DS for JSON serialization'''
+
+
+def class_to_json(obj):
+    '''returns the dict represenattion of a class'''
+    return obj.__dict__
+
+
+'''
+""" My class module
+"""
+
+class MyClass:
+    """ My class
+    """
+
+    def __init__(self, name):
+        self.name = name
+        self.number = 0
+
+    def __str__(self):
+        return "[MyClass] {} - {:d}".format(self.name, self.number)
+
+
+m = MyClass("John")
+m.number = 89
+print(type(m))
+print(m)
+
+mj = class_to_json(m)
+print(type(mj))
+print(mj)
+
+
+
+""" My class module
+"""
+
+class MyClass:
+    """ My class
+    """
+
+    score = 0
+
+    def __init__(self, name, number = 4):
+        self.__name = name
+        self.number = number
+        self.is_team_red = (self.number % 2) == 0
+
+    def win(self):
+        self.score += 1
+
+    def lose(self):
+        self.score -= 1
+
+    def __str__(self):
+        return "[MyClass] {} - {:d} => {:d}"
+        .format(self.__name, self.number, self.score)
+
+
+print("\n****************\n")
+
+m = MyClass("John")
+m.win()
+print(type(m))
+print(m)
+
+mj = class_to_json(m)
+print(type(mj))
+print(mj)
+'''
