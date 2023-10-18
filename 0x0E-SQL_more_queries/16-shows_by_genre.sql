77a46774bdeff75fe132d43627e6c947d9cb10f3
Grace <nzioki.grace@gmail.com>
2023-10-18 21:54:39 +0300

    list shows and genres

--- 0x0E-SQL_more_queries/16-shows_by_genre.sql
+++ 0x0E-SQL_more_queries/16-shows_by_genre.sql
@@ -1,8 +1,8 @@
 -- list all shows and all genres of it
-SELECT title, name FROM tv_shows
-  JOIN tv_show_genres
-    ON tv_shows.id = show_id
-  JOIN tv_genres
-    ON tv_genres.id = genre_id
+   SELECT title, name FROM tv_shows
+LEFT JOIN tv_show_genres
+       ON tv_shows.id = show_id
+LEFT JOIN tv_genres
+       ON tv_genres.id = genre_id
  ORDER BY title, name;
 
