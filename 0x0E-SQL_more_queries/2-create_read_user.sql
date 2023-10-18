292b826c8c25ae8f15266bd050d1bd8b9fe9702b
Grace <nzioki.grace@gmail.com>
2023-10-18 15:17:43 +0300

    create db, user and grant permissions

--- 0x0E-SQL_more_queries/2-create_read_user.sql
+++ 0x0E-SQL_more_queries/2-create_read_user.sql
@@ -2,8 +2,11 @@
 CREATE DATABASE IF NOT EXISTS hbtn_0d_2;
 
 --create user
-CREATE USER IF NOT EXISTS 'user_0d_2'@'localhost' IDENTIFIED BY 'user_0d_2_pwd';
+CREATE USER IF NOT EXISTS 'user_0d_2'@'localhost'
+            IDENTIFIED BY 'user_0d_2_pwd';
 
 -- grant permissions
-GRANT SELECT ON hbtn_0d_2.* TO 'user_0d_2'@'localhost';
+GRANT SELECT
+   ON hbtn_0d_2.*
+   TO 'user_0d_2'@'localhost';
 
