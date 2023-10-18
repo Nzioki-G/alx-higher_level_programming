89a6d07a4bf2f0080915918ba2f6b4630ee7f7ca
Grace <nzioki.grace@gmail.com>
2023-10-18 17:30:04 +0300

    get city of a certain state by id(retrievd in a subquery)

--- 0x0E-SQL_more_queries/8-cities_of_california_subquery.sql
+++ 0x0E-SQL_more_queries/8-cities_of_california_subquery.sql
@@ -1,8 +1,8 @@
 -- lists the cities of California in the db
 SELECT id, name
   FROM cities
-  WHERE id = (
-       SELECT state_id FROM states
+  WHERE state_id = (
+       SELECT id FROM states
         WHERE name = 'California')
   ORDER BY id ASC;
 
