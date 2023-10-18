49866ce41e274f2bf6c3924709c8ad01d7d1dd9e
Grace <nzioki.grace@gmail.com>
2023-10-18 21:21:55 +0300

    get tv genres count

---
+++ 0x0E-SQL_more_queries/13-count_shows_by_genre.sql
@@ -0,0 +1,7 @@
+-- lists all genres and shows linked to them
+SELECT name, COUNT(name) AS number_of_shows
+  FROM tv_genres JOIN tv_show_genres
+    ON id = genre_id
+ GROUP BY name
+ ORDER BY number_of_shows DESC;
+
